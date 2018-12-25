7946951b4fc2eaa75b44a13a9d69fe2333895b80
長谷川貴規 <hasegawatakashiki@net44-dhcp162.sfc.keio.ac.jp>
2018-12-25 09:57:48 +0900

    add

--- final2.js
+++ final2.js
@@ -1,6 +1,6 @@
-function question(){
+function question number(){
   var x = Number(document.getElementById("hoge").value;)
   for (var i = 0; i < x; i++){
-
+var x =document.getElementById('clone')
   }
 }
