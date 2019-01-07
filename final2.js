d468d2c643afe8d02e3fef3fa4329ef97fe8a9ec
長谷川貴規 <hasegawatakashiki@hasegawatakashikinoMacBook-ea.local>
2019-01-07 19:31:33 +0900

    add

--- final2.js
+++ final2.js
@@ -1,6 +1,8 @@
-function question number(){
-  var x = Number(document.getElementById("hoge").value;)
-  for (var i = 0; i < x; i++){
-var x =document.getElementById('clone')
+function question_number(){
+  var x = Number(document.getElementById("hoge").value)
+  for (var i = 1; i < x; i++){
+var clone = document.getElementById('clone')
+clone.innerHTML += "問題　　：<input id='question' + i + 'type='text' placeholder='問題' size='40''><br>"
+clone.innerHTML += "答え　　：<input id='answer' type='text' placeholder='解答欄'><br>"
   }
 }
