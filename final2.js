ed173bee92bbf95bc68c0d290eabd2c70580e6e8
長谷川貴規 <hasegawatakashiki@net44-dhcp80.sfc.keio.ac.jp>
2019-01-09 09:29:07 +0900

    add

--- final2.js
+++ final2.js
@@ -1,8 +1,15 @@
 function question_number(){
   var x = Number(document.getElementById("hoge").value)
-  for (var i = 1; i < x; i++){
+  for (var i = 0; i < x; i++){
 var clone = document.getElementById('clone')
-clone.innerHTML += "問題　　：<input id='question' + i + 'type='text' placeholder='問題' size='40''><br>"
-clone.innerHTML += "答え　　：<input id='answer' type='text' placeholder='解答欄'><br>"
+clone.innerHTML += "問題　　：<input id='question" + i + "'type='text' placeholder='問題' size='40'><br>"
+clone.innerHTML += "答え　　：<input id='answer" + i + "' type='text' placeholder='解答欄'><br>"
   }
+}
+function createQuestion(){
+  var answer = []
+    answer.push(document.getElementById('clone').value)
+}
+function alert(){
+  alert(answer[1])
 }
