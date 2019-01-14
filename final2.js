62793cd5783761844230e2905b0e11c6def21060
長谷川貴規 <hasegawatakashiki@hasegawatakashikinoMacBook-ea.local>
2019-01-14 19:29:50 +0900

    add

--- final2.js
+++ final2.js
@@ -11,14 +11,27 @@ var answer = [];
 function createQuestion(){
   var question = document.getElementById('question')
   var x = Number(document.getElementById("hoge").value)
+
   for (var i = 0; i < x; i++){
   answer.push(document.getElementById('answer' + i).value);
-  question.innerHTML += "<input id='answer1-item' type='text' placeholder='解答欄'><br>"
-  question.innerHTML += "<input type='button' value='回答' onclick='hoge" + i + "();'><br>"
+ question.innerHTML += document.getElementById('question' + i).value;
+ question.innerHTML += "<p><input id='answer_item" + i + "' type='text' placeholder='解答欄'></p>"
+}
+document.getElementById('allquestion').innerHTML = "<h2>'Question!'</h2>"
+document.getElementById('result').innerHTML = "<input type='button' value='result' onclick='result();'>"
+}
 
-}}
+var score = 0;
+var answer_item = [];
 
-
-function a(){
-  console.log(answer)
+function result(){
+  var x = Number(document.getElementById("hoge").value)
+score = 0
+  for(var i = 0; i < x; i++){
+    answer_item.push(document.getElementById('answer_item' + i).value)
+    if(answer[i] == answer_item[i]){
+      score += 1
+    }
+  }
+  alert('あなたのスコアは' + score + '点です！')
 }
